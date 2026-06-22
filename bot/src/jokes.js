5baac55898f36c49baf9b6cd02b1b9eec2a09232
arsen <dreamofars@gmail.com>
2026-06-23 01:25:58 +0400

    fixing bugs #1 (object-object)

--- bot/src/jokes.js
+++ bot/src/jokes.js
@@ -5,7 +5,7 @@ const JOKES_PATH = path.join(__dirname, '../content/jokes.json');
 const jokes = JSON.parse(fs.readFileSync(JOKES_PATH, 'utf-8'));
 
 function getRandomJoke() {
-    return jokes[Math.floor(Math.random() * jokes.length)];
+    return jokes[Math.floor(Math.random() * jokes.length)].text;
 }
 
 module.exports = { getRandomJoke };
